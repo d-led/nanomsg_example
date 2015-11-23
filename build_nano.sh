56cb599a5ee650b97ed53d8ed96fce16c88bac92
Dmitry Ledentsov <dmlled@yahoo.com>
2015-11-23 21:01:45 +0100

    switch to the latest nanomsg

--- build_nano.sh
+++ build_nano.sh
@@ -1,7 +1,7 @@
-wget https://github.com/nanomsg/nanomsg/releases/download/0.6-beta/nanomsg-0.6-beta.tar.gz -P tmp
+wget https://github.com/nanomsg/nanomsg/releases/download/0.8-beta/nanomsg-0.8-beta.tar.gz -P tmp
 cd tmp
-tar -xzvf nanomsg-0.6-beta.tar.gz
-./nanomsg-0.6-beta/configure --prefix=`pwd`/../nano
+tar -xzvf nanomsg-0.8-beta.tar.gz
+./nanomsg-0.8-beta/configure --prefix=`pwd`/../nano
 make
 make install
 cd ..
